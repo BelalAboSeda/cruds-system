6077beaf2b0becf3b364251e85a1e8954f43707d
BelalAboSeda <126694241+BelalAboSeda@users.noreply.github.com>
2023-07-28 04:05:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,210 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count')
+let category = document.getElementById('category');
+let create = document.getElementById('create');
+let mood = 'create';
+let up;
+//get total price
+function getTotal() {
+    if (price.value != '') {
+        let result = (+price.value + +taxes.value + +ads.value)
+            - +discount.value;
+        total.innerHTML = result;
+        total.style.backgroundColor = '#040';
+    } else {
+        total.innerHTML = '';
+        total.style.backgroundColor = '#e50000'
+    }
+}
+//create product 
+let dataPro;
+if (localStorage.product != null) {
+    dataPro = JSON.parse(localStorage.product)
+} else {
+    dataPro = [];
+}
+create.onclick = function () {
+    let newPro = {
+        title: title.value.toLowerCase(),
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        total: total.innerHTML,
+        count: count.value,
+        category: category.toLowerCase(),
+    }
+    if (mood == 'create') {
+        if (newPro.count > 1) {
+            for (let i = 0; i < newPro.count; i++) {
+                dataPro.push(newPro)
+            }
+        } else {
+            dataPro.push(newPro)
+        }
+    } else {
+        dataPro[up] = newPro;
+        create.innerHTML = 'Create';
+        mood = 'create';
+        count.style.display = '';
+    }
+    localStorage.setItem('product', JSON.stringify(dataPro))
+    clearData()
+    showData()
+}
+//clear Data
+
+function clearData() {
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    count.value = '';
+    category.value = '';
+}
+
+
+//read
+function showData() {
+    getTotal()
+    let table = '';
+    for (let i = 0; i < dataPro.length; i++) {
+        table += `
+        <tr>
+           <td>${i}</td>
+           <td>${dataPro[i].title}</td>
+           <td>${dataPro[i].price}</td>
+            <td>${dataPro[i].taxes}</td>
+            <td>${dataPro[i].ads}</td>
+            <td>${dataPro[i].discount}</td>
+            <td>${dataPro[i].total}</td>
+            <td>${dataPro[i].category}</td>
+            <td><button onclick="updateData(${i})" class="button" id="update">update</button></td>
+            <td>
+                <button onclick="delData(${i})" class="btn-del" type="button">
+                    <span class="button__text">Delete</span>
+                    <span class="button__icon"><svg class="svg" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M112,112l20,320c.95,18.49,14.4,32,32,32H348c17.67,0,30.87-13.51,32-32l20-320" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"></path><line style="stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px" x1="80" x2="432" y1="112" y2="112"></line><path d="M192,112V72h0a23.93,23.93,0,0,1,24-24h80a23.93,23.93,0,0,1,24,24h0v40" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"></path><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="256" x2="256" y1="176" y2="400"></line><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="184" x2="192" y1="176" y2="400"></line><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="328" x2="320" y1="176" y2="400"></line></svg></span>
+                  </button>
+            </td>
+                    </tr>
+        `
+    }
+    document.getElementById('tbody').innerHTML = table;
+    let btnDelete = document.getElementById('deleteAll')
+    if (dataPro.length > 0) {
+        btnDelete.innerHTML = `
+        <button onclick="deleteAll()" class="button" id="btnDel" type="button">
+         Delete All(${dataPro.length})
+         </button>
+        `
+    }
+}
+showData()
+
+//del Data
+function delData(i) {
+    dataPro.splice(i, 1);
+    localStorage.product = JSON.stringify(dataPro);
+    showData()
+}
+
+function deleteAll() {
+    localStorage.clear()
+    dataPro.splice(0)
+    showData()
+}
+//update data
+function updateData(i) {
+    title.value = dataPro[i].title;
+    price.value = dataPro[i].price;
+    taxes.value = dataPro[i].taxes;
+    ads.value = dataPro[i].ads;
+    discount.value = dataPro[i].discount;
+    getTotal();
+    count.style.display = 'none';
+    category.value = dataPro[i].category;
+    create.innerHTML = 'Update';
+    mood = 'update';
+    up = i;
+    scroll({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+//searchMood
+let searchMood = 'title';
+
+function getSearchMood(id) {
+    let search = document.getElementById('search')
+    if (id === 'searchTitle') {
+        searchMood = 'title';
+    } else {
+        searchMood = 'category';
+    }
+    search.placeholder = 'Search By ' + searchMood;
+    search.focus()
+    search.value = '';
+    showData()
+}
+
+//search
+
+function searchData(value) {
+    let table = '';
+    for (let i = 0; i < dataPro.length; i++) {
+        if (searchMood === 'title') {
+            if (dataPro[i].title.includes(value.toLowerCase())) {
+                table += `
+        <tr>
+           <td>${i}</td>
+           <td>${dataPro[i].title}</td>
+           <td>${dataPro[i].price}</td>
+            <td>${dataPro[i].taxes}</td>
+            <td>${dataPro[i].ads}</td>
+            <td>${dataPro[i].discount}</td>
+            <td>${dataPro[i].total}</td>
+            <td>${dataPro[i].category}</td>
+            <td><button onclick="updateData(${i})" class="button" id="update">update</button></td>
+            <td>
+                <button onclick="delData(${i})" class="btn-del" type="button">
+                    <span class="button__text">Delete</span>
+                    <span class="button__icon"><svg class="svg" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M112,112l20,320c.95,18.49,14.4,32,32,32H348c17.67,0,30.87-13.51,32-32l20-320" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"></path><line style="stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px" x1="80" x2="432" y1="112" y2="112"></line><path d="M192,112V72h0a23.93,23.93,0,0,1,24-24h80a23.93,23.93,0,0,1,24,24h0v40" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"></path><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="256" x2="256" y1="176" y2="400"></line><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="184" x2="192" y1="176" y2="400"></line><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="328" x2="320" y1="176" y2="400"></line></svg></span>
+                  </button>
+            </td>
+                    </tr>
+        `
+            }
+        } else {
+            if (dataPro[i].category.includes(value.toLowerCase())) {
+                table += `
+        <tr>
+           <td>${i}</td>
+           <td>${dataPro[i].title}</td>
+           <td>${dataPro[i].price}</td>
+            <td>${dataPro[i].taxes}</td>
+            <td>${dataPro[i].ads}</td>
+            <td>${dataPro[i].discount}</td>
+            <td>${dataPro[i].total}</td>
+            <td>${dataPro[i].category}</td>
+            <td><button onclick="updateData(${i})" class="button" id="update">update</button></td>
+            <td>
+                <button onclick="delData(${i})" class="btn-del" type="button">
+                    <span class="button__text">Delete</span>
+                    <span class="button__icon"><svg class="svg" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M112,112l20,320c.95,18.49,14.4,32,32,32H348c17.67,0,30.87-13.51,32-32l20-320" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"></path><line style="stroke:#fff;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px" x1="80" x2="432" y1="112" y2="112"></line><path d="M192,112V72h0a23.93,23.93,0,0,1,24-24h80a23.93,23.93,0,0,1,24,24h0v40" style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"></path><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="256" x2="256" y1="176" y2="400"></line><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="184" x2="192" y1="176" y2="400"></line><line style="fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" x1="328" x2="320" y1="176" y2="400"></line></svg></span>
+                  </button>
+            </td>
+                    </tr>
+        `
+            }
+        }
+        document.getElementById('tbody').innerHTML = table;
+    }
+}
